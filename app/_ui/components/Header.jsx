c109fa2cd8890a93cdffd9ec5d2251709c12729a
Dany Mrushi <mrushidany@gmail.com>
2024-03-25 16:57:55 +0300

    Customized and setup the reusable header component

--- app/_ui/components/Header.jsx
+++ app/_ui/components/Header.jsx
@@ -1,9 +1,14 @@
 'use client'
 
+import { Helmet } from "react-helmet"
+
 export default function Header({ title, description }) {
   return (
     <>
-        
+        <Helmet>
+            <title>{title}</title>
+            <meta description={description} />
+        </Helmet>
     </>
   )
 }
