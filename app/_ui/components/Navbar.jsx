91b0a4e6cd29de4952b953f2007a6642616461eb
Dany Mrushi <mrushidany@gmail.com>
2024-03-25 17:05:54 +0300

    Integrated the navbar into the main section

--- app/_ui/components/Navbar.jsx
+++ app/_ui/components/Navbar.jsx
@@ -2,8 +2,12 @@
 
 export default function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='w-full h-[62px] flex items-center px-20'>
+        <nav className='flex flex-row justify-between'>
+
+        </nav>
+      </div>
+    </>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "./_ui/components/Navbar";
 
 export default function Home() {
   return (
     <main className="w-full bg-black-default min-h-screen font-poppins">
-    
+      <Navbar />
     </main>
   )
 }
