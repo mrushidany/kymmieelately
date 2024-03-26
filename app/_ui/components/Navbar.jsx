136ed33220de5fb217f0e3cdf7a73acd9ca95b8d
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 09:48:33 +0300

    Updated the favicon

--- app/_ui/components/Navbar.jsx
+++ app/_ui/components/Navbar.jsx
@@ -1,13 +1,14 @@
 'use client'
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import Image from 'next/image'
 
 export default function Navbar() {
     const pathname = usePathname()
   return (
     <>
       <div className='w-full h-[102px] flex items-center px-40'>
-        <nav className='flex flex-row justify-between'>
+        <nav className='flex flex-row  items-center justify-between'>
             <ul className='flex flex-row gap-x-8 text-white-default uppercase text-[13px] font-semibold tracking-[1.2px]'>
                 <li>
                     <Link href='/' className={`${pathname === '/' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>Home</Link>
@@ -19,6 +20,9 @@ export default function Navbar() {
                     <Link href='#' className={`${pathname === '/blog' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>Blog</Link>
                 </li>
             </ul>
+            <div>
+                <Image src='/images/icon.webp' priority className='w-[221.47px] h-[63.27px] object-cover' width={10} height={10} />
+            </div>
         </nav>
       </div>
     </>
