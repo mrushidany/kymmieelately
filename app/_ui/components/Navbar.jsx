7413a67beb82cc1ed7093a556c03bca488275ab3
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 09:54:18 +0300

    Navbar setup

--- app/_ui/components/Navbar.jsx
+++ app/_ui/components/Navbar.jsx
@@ -8,21 +8,32 @@ export default function Navbar() {
   return (
     <>
       <div className='w-full h-[102px] flex items-center px-40'>
-        <nav className='flex flex-row  items-center justify-between'>
-            <ul className='flex flex-row gap-x-8 text-white-default uppercase text-[13px] font-semibold tracking-[1.2px]'>
-                <li>
-                    <Link href='/' className={`${pathname === '/' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>Home</Link>
-                </li>
-                <li>
-                    <Link href='#' className={`${pathname === '/about' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>About</Link>
-                </li>
-                <li>
-                    <Link href='#' className={`${pathname === '/blog' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>Blog</Link>
-                </li>
-            </ul>
-            <div>
-                <Image src='/images/icon.webp' priority className='w-[221.47px] h-[63.27px] object-cover' width={10} height={10} />
-            </div>
+        <nav className='w-full flex flex-row items-center justify-between'>
+          <ul className='flex flex-row gap-x-8 text-white-default uppercase text-[13px] font-semibold tracking-[1.2px]'>
+            <li>
+              <Link href='/' className={`${pathname === '/' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>Home</Link>
+            </li>
+            <li>
+              <Link href='#' className={`${pathname === '/about' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>About</Link>
+            </li>
+            <li>
+              <Link href='#' className={`${pathname === '/blog' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>Blog</Link>
+            </li>
+          </ul>
+          <Link href='/'>
+            <img src='/images/icon.webp' priority className='w-[221.47px] h-[63.27px] object-cover' width={0} height={0} />
+          </Link>
+          <ul className='flex flex-row gap-x-8 text-white-default uppercase text-[13px] font-semibold tracking-[1.2px]'>
+            <li>
+              <Link href='#' className={`${pathname === '/youtube' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>Youtube</Link>
+            </li>
+            <li>
+              <Link href='#' className={`${pathname === '/podcast' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>Podcast</Link>
+            </li>
+            <li>
+              <Link href='#' className={`${pathname === '/contact-us' ? ' text-brown-default' : 'text-white-default hover:text-brown-default'}`}>Contact us</Link>
+            </li>
+          </ul>
         </nav>
       </div>
     </>
