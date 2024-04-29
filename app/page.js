ef6e549a0d863600933867aca90e4b9516b132f8
Dany Mrushi <mrushidany@gmail.com>
2024-04-29 10:51:46 +0300

    Customize the hero section

--- app/page.js
+++ app/page.js
@@ -10,13 +10,18 @@ export default function Home() {
   return (
     <main className="w-full bg-black-default min-h-screen font-poppins">
       <Navbar />
-      <div className={`w-full h-screen grid grid-cols-1 lg:grid-cols-4 bg-transparent`}>
-        {categories.map((item, index) => (
-          <div key={index} className={`${index == (categories.length - 1) ? 'border-b-0 lg:border-r-0' : 'border-b-[1px] lg:border-r-[0.5px] lg:border-b-0 border-gray-default'} flex flex-col gap-2 px-4 py-10 uppercase`}>
-            <p className='text-[11px]'>Category</p>
-            <p className='font-teko font-medium text-[21px] leading-6 tracking-2'>{item.name}</p>
-          </div>
-        ))}
+      <div className='w-full h-screen'>
+        <div className={`w-full h-full lg:h-3/4 grid grid-cols-1 lg:grid-cols-4 bg-transparent`}>
+          {categories.map((item, index) => (
+            <div key={index} className={`${index == (categories.length - 1) ? 'border-b-0 lg:border-r-0' : 'border-b-[1px] lg:border-r-[0.5px] lg:border-b-0 border-gray-default'} flex flex-col gap-2 px-4 py-10 uppercase`}>
+              <p className='text-[11px]'>Category</p>
+              <p className='font-teko font-medium text-[21px] leading-6 tracking-2'>{item.name}</p>
+            </div>
+          ))}
+        </div>
+        <div className='hidden lg:block w-full h-1/4 bg-white-default'>
+
+        </div>
       </div>
     </main>
   )
