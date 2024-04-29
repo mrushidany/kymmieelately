657c490b6e11ca0e031afcfd9ca0a541858b92fe
Dany Mrushi <mrushidany@gmail.com>
2024-04-29 10:48:28 +0300

    Updated the border color

--- app/page.js
+++ app/page.js
@@ -10,9 +10,9 @@ export default function Home() {
   return (
     <main className="w-full bg-black-default min-h-screen font-poppins">
       <Navbar />
-      <div className={`w-full h-screen grid grid-cols-1 lg:grid-cols-${categories.length} bg-transparent`}>
+      <div className={`w-full h-screen grid grid-cols-1 lg:grid-cols-4 bg-transparent`}>
         {categories.map((item, index) => (
-          <div key={index} className={`${index == (categories.length - 1) ? '' : 'border-b-[1px] lg:border-r-[1px] border-solid border-brown-200'} flex flex-col gap-2 px-4 py-10 uppercase`}>
+          <div key={index} className={`${index == (categories.length - 1) ? 'border-b-0 lg:border-r-0' : 'border-b-[1px] lg:border-r-[0.5px] lg:border-b-0 border-gray-default'} flex flex-col gap-2 px-4 py-10 uppercase`}>
             <p className='text-[11px]'>Category</p>
             <p className='font-teko font-medium text-[21px] leading-6 tracking-2'>{item.name}</p>
           </div>
