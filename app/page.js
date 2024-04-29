01ce08696a65768d2717ccb03ffce2da4c931b27
Dany Mrushi <mrushidany@gmail.com>
2024-04-29 10:37:53 +0300

    Responsive borders

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Navbar />
       <div className={`w-full h-screen grid grid-cols-1 lg:grid-cols-${categories.length} bg-transparent`}>
         {categories.map((item, index) => (
-          <div key={index} className={`${index == (categories.length - 1) ? '' : 'border-r-[1px] border-solid border-brown-200'} flex flex-col gap-2 px-4 py-10 uppercase`}>
+          <div key={index} className={`${index == (categories.length - 1) ? '' : 'border-b-[1px] lg:border-r-[1px] border-solid border-brown-200'} flex flex-col gap-2 px-4 py-10 uppercase`}>
             <p className='text-[11px]'>Category</p>
             <p className='font-teko font-medium text-[21px] leading-6 tracking-2'>{item.name}</p>
           </div>
