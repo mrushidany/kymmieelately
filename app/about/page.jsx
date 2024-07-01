0b60428ac8d09b744692016cf5c7f42a278d1809
Dany Mrushi <mrushidany@gmail.com>
2024-07-01 16:49:10 +0300

    Setup the video in about section

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -7,6 +7,13 @@ export default function AboutPage() {
     <>
       <section className='w-screen bg-black-default min-h-screen'>
         <Navbar />
+        <div className='w-full h-screen relative'>
+          <div className='absolute inset-0 overflow-hidden'>
+            <video poster='/images/bg/2.png' autoPlay loop muted playsInline className='w-full h-full object-cover'>
+              <source src='/videos/helicopter.mp4' type="video/mp4" />
+            </video>
+          </div>
+        </div>
       </section>
     </>
   )
